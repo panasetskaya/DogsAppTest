abdf89d1222d82c3354a0830f05e55f4d1993e62
Maria Panasetskaia <mariapolikutina@gmail.com>
2023-03-16 15:31:31 +0100

    add basic structure, add colors

---
+++ app/src/main/java/com/panasetskaia/dogsapp/domain/DogRepository.kt
@@ -0,0 +1,4 @@
+package com.panasetskaia.dogsapp.domain
+
+interface DogRepository {
+}
--- app/src/main/java/com/panasetskaia/dogsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/panasetskaia/dogsapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.panasetskaia.dogsapp
+package com.panasetskaia.dogsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.panasetskaia.dogsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
